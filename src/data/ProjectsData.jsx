f3dd61a5e0b28fc274b2bf6660809233974c81d6
Fred Mark Baldeviso <fredmark.baldeviso@gmail.com>
2024-10-31 23:21:08 +0800

    Adds more project preview images
    Update images, uses images with lower file sizes

--- src/data/ProjectsData.jsx
+++ src/data/ProjectsData.jsx
@@ -1,9 +1,17 @@
-import shoppinCartPreview from '../assets/projects-preview/shopping-cart-sprite.png'
-import memoryCardPreview from '../assets/projects-preview/memory-card-sprite.png';
-import rockPaperScissorsPreview from '../assets/projects-preview/rock-paper-scissors-sprite.png'
-import etchASketchPreview from '../assets/projects-preview/etch-a-sketch-sprite.png';
-import calculatorPreview from '../assets/projects-preview/calculator-sprite.png';
-import signUpFormPreview from '../assets/projects-preview/sign-up-form-sprite.png'
+import rockPaperScissorsPreview from '../assets/projects-preview/rock-paper-scissors.png'
+import etchASketchPreview from '../assets/projects-preview/etch-a-sketch.png';
+import calculatorPreview from '../assets/projects-preview/calculator.png';
+import signUpFormPreview from '../assets/projects-preview/sign-up-form.png';
+import adminDashboardPreview from '../assets/projects-preview/admin-dashboard.png';
+import libraryPreview from '../assets/projects-preview/library.png';
+import ticTacToePreview from '../assets/projects-preview/tic-tac-toe.png';
+import restaurantPreview from '../assets/projects-preview/restaurant-page.png';
+import todoListPreview from '../assets/projects-preview/todo-list.png';
+import weatherAppPreview from '../assets/projects-preview/weather-app.png';
+import battleshipPreview from '../assets/projects-preview/battleship.png';
+import cvApplicationPreview from '../assets/projects-preview/cv-application.png';
+import memoryCardPreview from '../assets/projects-preview/memory-card.png';
+import shoppinCartPreview from '../assets/projects-preview/shopping-cart.png'
 
 const projectsArr = [];
 class Project {
@@ -42,7 +50,7 @@ const memoryCard = new Project(
 const cvApplication = new Project(
   3,
   'CV Application',
-  memoryCardPreview,
+  cvApplicationPreview,
   'A Curriculum Vitae maker web app. Create a CV using a template by filling up form input fields.',
   'https://github.com/makieldeviso/CV-Application',
   'https://cv-application-1lb.pages.dev/',
@@ -52,7 +60,7 @@ const cvApplication = new Project(
 const battleship = new Project(
   4,
   'Battleship',
-  memoryCardPreview,
+  battleshipPreview,
   'A battleship game that runs on the browser',
   'https://github.com/makieldeviso/battleship',
   'https://makieldeviso.github.io/battleship/',
@@ -62,7 +70,7 @@ const battleship = new Project(
 const weatherApp = new Project(
   5,
   'Weather App',
-  memoryCardPreview,
+  weatherAppPreview,
   'Weather App using free tier WeatherAPI. Enter location and check its real-time weather and forecast information in a responsive app display.',
   'https://github.com/makieldeviso/weather-app',
   'https://makieldeviso.github.io/weather-app/',
@@ -72,7 +80,7 @@ const weatherApp = new Project(
 const todoList = new Project(
   6,
   'Todo List',
-  memoryCardPreview,
+  todoListPreview,
   'A Todo List App that can create events with tasks, and projects that can store events. Can track and filter schedule/ deadline of affairs and todo level of priority.',
   'https://github.com/makieldeviso/todo-list',
   'https://makieldeviso.github.io/todo-list/',
@@ -82,7 +90,7 @@ const todoList = new Project(
 const restaurantPage = new Project(
   7,
   'Restaurant Page',
-  memoryCardPreview,
+  restaurantPreview,
   'Restaurant page for Heartbreak Pub portfolio project.',
   'https://github.com/makieldeviso/restaurant',
   'https://makieldeviso.github.io/restaurant/',
@@ -92,7 +100,7 @@ const restaurantPage = new Project(
 const tictactoe = new Project(
   8,
   'Tic-tac-toe',
-  memoryCardPreview,
+  ticTacToePreview,
   'A tic-tac-toe game featuring 3 modes, a two-player versus mode, a computer opponent with randomized moves and an unbeatable computer opponent equipped with minimax algorithm.',
   'https://github.com/makieldeviso/tic-tac-toe',
   'https://makieldeviso.github.io/tic-tac-toe/',
@@ -102,7 +110,7 @@ const tictactoe = new Project(
 const library = new Project (
   9,
   'Library',
-  memoryCardPreview,
+  libraryPreview,
   'A library web app portfolio project titled myReadList that can add book information, then displays to a digital catalog.',
   'https://github.com/makieldeviso/library',
   'https://makieldeviso.github.io/library/',
@@ -112,7 +120,7 @@ const library = new Project (
 const adminDashboard = new Project (
   10,
   'Admin Dashboard',
-  memoryCardPreview,
+  adminDashboardPreview,
   'An admin dashboard for a company named Trash Panda Co. portfolio project. Exemplify web layout design using CSS, emphasizing use of Flex and Grid display rules.',
   'https://github.com/makieldeviso/admin-dashboard',
   'https://makieldeviso.github.io/admin-dashboard/',
